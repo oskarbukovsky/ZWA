5d4906b6b8efdf55cb91de4985d6c105c726684e
oskarbukovsky <47369119+oskarbukovsky@users.noreply.github.com>
2024-10-14 01:03:40 +0200

    add debug thing and fix :after size when resizing

--- public/js/main.js
+++ public/js/main.js
@@ -58,7 +58,12 @@ window.addEventListener("DOMContentLoaded", async () => {
 
     cl("|📘 Finished in " + (new Date() - time1) + "ms");
     // 📕📙📗📘
-    return;
+
+    //Temporary FPS/frametime/ram usage counter
+
+    if (DEBUG) {
+        (function () { var script = document.createElement('script'); script.onload = function () { var stats = new Stats(); document.body.appendChild(stats.dom); requestAnimationFrame(function loop() { stats.update(); requestAnimationFrame(loop) }); }; script.src = 'https://mrdoob.github.io/stats.js/build/stats.min.js'; document.head.appendChild(script); })()
+    }
 });
 
 
